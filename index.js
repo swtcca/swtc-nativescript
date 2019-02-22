63b31cd0596e8a725765df036512f349a41e50e5
Lospring Liu <lospringliu@gmail.com>
2019-02-22 18:58:46 -0500

    make sure global.process.env exists

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 'use strict';
 
+try {
+    // make sure global.process.env exists for certain lib
+    global && global.process ?
+        global.process.env ?
+            "process.env exist"
+            : global.process = { env: {}}
+        : global.process = { env: {}}
+} catch (error) {
+}
+
 exports.aliases  = {
     'string_decoder': 'string_decoder/lib/string_decoder.js',
     'brorand$': 'swtc-nativescript-brorand',
